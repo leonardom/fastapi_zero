a159ff40aa5399aaf978b6e6223dec4ccfc00cf6
Leonardo Marcelino <leonardo.marcelino@goodlpay.com>
2025-10-22 19:43:54 +0100

    Setup sqlalchemy and alembic

---
+++ tests/test_db.py
@@ -0,0 +1,25 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as mock_time:
+        user = User(
+            username='testuser',
+            email='user@test.com',
+            password_hash='hashedpassword123',
+        )
+        session.add(user)
+        session.commit()
+        user = session.scalar(select(User).where(User.username == 'testuser'))
+        assert asdict(user) == {
+            'id': 1,
+            'username': 'testuser',
+            'email': 'user@test.com',
+            'password_hash': 'hashedpassword123',
+            'created_at': mock_time,
+            'updated_at': mock_time,
+        }
